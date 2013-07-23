8cc79f44a0b6b1f713fc8a56e266ff4d54a0471d
Jean Rouge <jer329@cornell.edu>
2013-07-22 21:36:56 -0700

    string list ok

--- xdebug_zomphp.c
+++ xdebug_zomphp.c
@@ -3,6 +3,98 @@
 	Author: Jean Rouge (jer329@cornell.edu)
  */
 
+#include <stdlib.h>
+#include <string.h>
+
 #include "xdebug_zomphp.h"
 
+#include <sys/time.h> // TODO wkpo
+
+// {{{ STRING_LIST }}}
+
+string_list* new_string_list()
+{
+	string_list* result;
+	result = (string_list*) malloc(sizeof(string_list));
+	if (!result) {
+		return NULL;
+	}
+	result->head = NULL;
+	result->tail = NULL;
+	return result;
+}
+
+void free_and_process_string_list(string_list* sl, process_string func)
+{
+	string_list_el *current, *next;
+	if (sl) {
+		next = sl->head;
+		while (next) {
+			current = next;
+			next = current->next;
+			if (func) {
+				func(current->data);
+			}
+			free(current->data);
+			free(current);
+		}
+		free(sl);
+	}
+}
+
+void add_string(string_list* sl, const char* s)
+{
+	string_list_el* new_el;
+	char* data;
+	if (!sl || !s) {
+		return;
+	}
+	data = strdup(s);
+	if (!data) {
+		return;
+	}
+	new_el = (string_list_el*) malloc(sizeof(string_list_el));
+	if (!new_el) {
+		free(data);
+		return;
+	}
+	new_el->data = data;
+	new_el->next = NULL;
+	if (!sl->head) {
+		sl->head = new_el;
+		sl->tail = new_el;
+	} else {
+		sl->tail->next = new_el;
+		sl->tail = new_el;
+	}
+}
+
+
+// {{{ END OF STRING_LIST }}}
+
+
+
+
+// TODO wkpo
+
+#include <stdio.h>
+void just_print(const char* s) {
+	printf("El: %s\n", s);
+}
+
+void test_sl()
+{
+	string_list* sl = new_string_list();
+	add_string(sl, "coucou");
+	add_string(sl, "wk");
+	add_string(sl, "po");
+	free_and_process_string_list(sl, just_print);
+	// free_and_process_string_list(sl, NULL);
+}
+
+int main()
+{
+	// test_sl();
+	return 0;
+}
 
--- xdebug_zomphp.h
+++ xdebug_zomphp.h
@@ -6,5 +6,30 @@
 #ifndef __HAVE_XDEBUG_ZOMPHP_H__
 #define __HAVE_XDEBUG_ZOMPHP_H__
 
+// I admittedly could have re-used the xdebug_llist struct here, but I prefer to stay as largely independent from
+// modifs in the xdebug code as possible (plus what I needed here is way simpler, and more efficient that way...
+// and hopefully without bug!)
+typedef struct string_list_el {
+	char* data;
+	struct string_list_el* next;
+} string_list_el;
+
+typedef struct string_list {
+	string_list_el* head;
+	string_list_el* tail;
+} string_list;
+
+typedef void (*process_string)(const char* s);
+
+string_list* new_string_list();
+void free_and_process_string_list(string_list* sl, process_string func);
+void add_string(string_list* sl, const char* s);
+
+
+// contains all the data: the functions seen so far, the lines we need to push during the next purge,
+// and when the last purge was
+typedef struct zomphp_data {
+	
+} zomphp_data;
 
 #endif
