a899bd607b39669f32a4fafa3ecbb949c2220c6c
Jean Rouge <rougej+github@gmail.com>
2026-01-11 03:29:19 -0800

    WIP

--- xdebug_stack.c
+++ xdebug_stack.c
@@ -1,4 +1,5 @@
 // wkpo idea: have a xdebug_2_5 branch in my repo (should be kept up to date with upstream 2.5),
+// wkpo make a PR with a clean diff to this
 
 /*
    +----------------------------------------------------------------------+
