a6ab3600b16addda728abdcd07ec0dd5b13ccbc7
Jean Rouge <rougej+github@gmail.com>
2026-03-30 16:25:20 -0700

    WIP

--- phuck_off_mmap.h
+++ phuck_off_mmap.h
@@ -8,19 +8,11 @@
 // Exposed so phuck_off_mmap_set() can stay as a tiny hot-path inline.
 extern unsigned char* phuck_off_mmap_bytes;
 
-#if defined(_MSC_VER)
-#define PHUCK_OFF_MMAP_INLINE static __inline
-#else
-#define PHUCK_OFF_MMAP_INLINE static inline
-#endif
-
 int phuck_off_mmap_init(const int n);
 void phuck_off_mmap_shutdown(void);
 
-PHUCK_OFF_MMAP_INLINE void phuck_off_mmap_set(const int i) {
+static inline void phuck_off_mmap_set(const int i) {
     phuck_off_mmap_bytes[((unsigned int) i) >> 3] |= (unsigned char) (1u << (((unsigned int) i) & 7u));
 }
-
-#undef PHUCK_OFF_MMAP_INLINE
 
 #endif
